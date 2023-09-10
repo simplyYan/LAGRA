5462c5ac8fb4f97da2f3cec0c36f0c6c693a14a6
Wesley Yan Soares Brehmer <102262759+NervousGroove@users.noreply.github.com>
2023-09-10 19:28:48 -0300

    Update lagra.go

--- lagra.go
+++ lagra.go
@@ -1,9 +1,12 @@
 package lagra
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"time"
+
+	"github.com/fatih/color"
 )
 
 type LogType string
@@ -22,28 +25,68 @@ func New() *Lagra {
 	return &Lagra{}
 }
 
-func (l *Lagra) send(logType LogType) func(message string) {
-	return func(message string) {
+func (l *Lagra) send(logType LogType) func(ctx context.Context, message string) error {
+	var textColor *color.Color
+
+	switch logType {
+	case Info:
+		textColor = color.New(color.FgGreen)
+	case Warn:
+		textColor = color.New(color.FgYellow)
+	case Error:
+		textColor = color.New(color.FgRed)
+	default:
+		textColor = color.New(color.Reset)
+	}
+
+	return func(ctx context.Context, message string) error {
 		logMessage := fmt.Sprintf("%s - %s - %s\n", time.Now().Format("15:04.05.9 - 02/01/2006"), logType, message)
-		fmt.Print(logMessage)
+		textColor.Print(logMessage)
+		fmt.Print(logMessage) // Imprime sem cores no arquivo de log
+
 		if l.logFile != nil {
-			l.logFile.WriteString(logMessage)
+			_, err := l.logFile.WriteString(logMessage)
+			if err != nil {
+				return err // Retornar erro se ocorrer um problema ao escrever no arquivo de log
+			}
 		}
+
+		return nil // Sem erro
 	}
 }
 
-func (l *Lagra) save() {
+// Info logs an informational message.
+func (l *Lagra) Info(ctx context.Context, message string) error {
+	return l.send(Info)(ctx, message)
+}
+
+// Warn logs a warning message.
+func (l *Lagra) Warn(ctx context.Context, message string) error {
+	return l.send(Warn)(ctx, message)
+}
+
+// Error logs an error message.
+func (l *Lagra) Error(ctx context.Context, message string) error {
+	return l.send(Error)(ctx, message)
+}
+
+func (l *Lagra) save() error {
 	if l.logFile == nil {
 		var err error
 		l.logFile, err = os.Create("log.lagra")
 		if err != nil {
-			fmt.Println("Error creating log file:", err)
+			return err // Retornar erro se ocorrer um problema ao criar o arquivo de log
 		}
 	}
+	return nil // Sem erro
 }
 
-func (l *Lagra) Close() {
+func (l *Lagra) Close() error {
 	if l.logFile != nil {
-		l.logFile.Close()
+		err := l.logFile.Close()
+		if err != nil {
+			return err // Retornar erro se ocorrer um problema ao fechar o arquivo de log
+		}
 	}
+	return nil // Sem erro
 }
