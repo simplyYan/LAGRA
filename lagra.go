7eca9a75091d476749f70f39d8829865f9b4df38
Wesley Yan Soares Brehmer <102262759+NervousGroove@users.noreply.github.com>
2023-09-10 20:47:35 -0300

    Update lagra.go

--- lagra.go
+++ lagra.go
@@ -1,101 +1,136 @@
 package lagra
 
 import (
-    "context"
-    "fmt"
-    "os"
-    "sync"
-    "time"
+	"context"
+	"fmt"
+	"os"
+	"sync"
+	"time"
 
-    "github.com/fatih/color"
+	"github.com/BurntSushi/toml"
+	"github.com/fatih/color"
 )
 
 type LogType string
 
 const (
-    Info  LogType = "INFO"
-    Warn  LogType = "WARN"
-    Error LogType = "ERROR"
+	Info  LogType = "INFO"
+	Warn  LogType = "WARN"
+	Error LogType = "ERROR"
 )
 
 type Lagra struct {
-    logFile  *os.File
-    logMutex sync.Mutex
+	logFile  *os.File
+	logMutex sync.Mutex
+	config   *LagraConfig
 }
 
-func New() *Lagra {
-    return &Lagra{}
+type LagraConfig struct {
+	LogFile  string `toml:"log_file"`
+	LogLevel string `toml:"log_level"`
 }
 
+// New creates a new Lagra logger instance with optional TOML configuration.
+func New(tomlConfig string) (*Lagra, error) {
+	var config LagraConfig
+	if tomlConfig != "" {
+		if _, err := toml.Decode(tomlConfig, &config); err != nil {
+			return nil, err
+		}
+	}
+
+	l := &Lagra{
+		config: &config,
+	}
+
+	if l.config.LogFile != "" {
+		l.SetLogFile(l.config.LogFile)
+	}
+
+	if l.config.LogLevel != "" {
+		l.SetLogLevel(l.config.LogLevel)
+	}
+
+	return l, nil
+}
+
+// send logs a message with the specified log type.
 func (l *Lagra) send(logType LogType, message string) error {
-    l.logMutex.Lock()
-    defer l.logMutex.Unlock()
+	l.logMutex.Lock()
+	defer l.logMutex.Unlock()
 
-    logMessage := fmt.Sprintf("%s - %s - %s\n", time.Now().Format("15:04.05.9 - 02/01/2006"), logType, message)
+	logMessage := fmt.Sprintf("%s - %s - %s\n", time.Now().Format("15:04.05.9 - 02/01/2006"), logType, message)
 
-    var textColor *color.Color
-    switch logType {
-    case Info:
-        textColor = color.New(color.FgGreen)
-    case Warn:
-        textColor = color.New(color.FgYellow)
-    case Error:
-        textColor = color.New(color.FgRed)
-    default:
-        textColor = color.New(color.Reset)
-    }
+	var textColor *color.Color
+	switch logType {
+	case Info:
+		textColor = color.New(color.FgGreen)
+	case Warn:
+		textColor = color.New(color.FgYellow)
+	case Error:
+		textColor = color.New(color.FgRed)
+	default:
+		textColor = color.New(color.Reset)
+	}
 
-    logMessageColored := textColor.SprintFunc()(logMessage)
-    fmt.Print(logMessageColored) // Imprime com cores
+	logMessageColored := textColor.SprintFunc()(logMessage)
+	fmt.Print(logMessageColored) // Imprime com cores
 
-    if l.logFile != nil {
-        _, err := l.logFile.WriteString(logMessage)
-        if err != nil {
-            return err // Retornar erro se ocorrer um problema ao escrever no arquivo de log
-        }
-    }
+	if l.logFile != nil {
+		_, err := l.logFile.WriteString(logMessage)
+		if err != nil {
+			return err // Retornar erro se ocorrer um problema ao escrever no arquivo de log
+		}
+	}
 
-    return nil // Sem erro
+	return nil // Sem erro
 }
 
 // Info logs an informational message.
 func (l *Lagra) Info(ctx context.Context, message string) error {
-    return l.send(Info, message)
+	return l.send(Info, message)
 }
 
 // Warn logs a warning message.
 func (l *Lagra) Warn(ctx context.Context, message string) error {
-    return l.send(Warn, message)
+	return l.send(Warn, message)
 }
 
 // Error logs an error message.
 func (l *Lagra) Error(ctx context.Context, message string) error {
-    return l.send(Error, message)
+	return l.send(Error, message)
 }
 
-func (l *Lagra) save() error {
-    l.logMutex.Lock()
-    defer l.logMutex.Unlock()
+// SetLogFile sets the log file path.
+func (l *Lagra) SetLogFile(filePath string) {
+	l.logMutex.Lock()
+	defer l.logMutex.Unlock()
 
-    if l.logFile == nil {
-        var err error
-        l.logFile, err = os.Create("log.lagra")
-        if err != nil {
-            return err // Retornar erro se ocorrer um problema ao criar o arquivo de log
-        }
-    }
-    return nil // Sem erro
+	if l.logFile != nil {
+		_ = l.logFile.Close()
+	}
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		fmt.Printf("Failed to create log file: %v\n", err)
+		return
+	}
+
+	l.logFile = file
 }
 
-func (l *Lagra) Close() error {
-    l.logMutex.Lock()
-    defer l.logMutex.Unlock()
+// SetLogLevel sets the log level (INFO, WARN, ERROR).
+func (l *Lagra) SetLogLevel(level string) {
+	switch level {
+	case "INFO":
+		// Set log level to INFO
+	case "WARN":
+		// Set log level to WARN
+	case "ERROR":
+		// Set log level to ERROR
+	default:
+		level = "INFO" // Default log level
+	}
 
-    if l.logFile != nil {
-        err := l.logFile.Close()
-        if err != nil {
-            return err // Retornar erro se ocorrer um problema ao fechar o arquivo de log
-        }
-    }
-    return nil // Sem erro
+	fmt.Printf("Log level set to %s\n", level)
 }
