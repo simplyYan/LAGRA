125e9f76f5e3dcc1b2cd5e4ccd5d052aa796353c
Wesley Yan Soares Brehmer <102262759+NervousGroove@users.noreply.github.com>
2023-09-10 20:13:53 -0300

    Update lagra.go

--- lagra.go
+++ lagra.go
@@ -1,103 +1,101 @@
 package lagra
 
 import (
-	"context"
-	"fmt"
-	"os"
-	"sync"
-	"time"
+    "context"
+    "fmt"
+    "os"
+    "sync"
+    "time"
 
-	"github.com/fatih/color"
+    "github.com/fatih/color"
 )
 
 type LogType string
 
 const (
-	Info  LogType = "INFO"
-	Warn  LogType = "WARN"
-	Error LogType = "ERROR"
+    Info  LogType = "INFO"
+    Warn  LogType = "WARN"
+    Error LogType = "ERROR"
 )
 
 type Lagra struct {
-	logFile  *os.File
-	logMutex sync.Mutex
+    logFile  *os.File
+    logMutex sync.Mutex
 }
 
 func New() *Lagra {
-	return &Lagra{}
+    return &Lagra{}
 }
 
-func (l *Lagra) send(logType LogType) func(ctx context.Context, message string) error {
-	return func(ctx context.Context, message string) error {
-		l.logMutex.Lock()
-		defer l.logMutex.Unlock()
+func (l *Lagra) send(logType LogType, message string) error {
+    l.logMutex.Lock()
+    defer l.logMutex.Unlock()
 
-		logMessage := fmt.Sprintf("%s - %s - %s\n", time.Now().Format("15:04.05.9 - 02/01/2006"), logType, message)
+    logMessage := fmt.Sprintf("%s - %s - %s\n", time.Now().Format("15:04.05.9 - 02/01/2006"), logType, message)
 
-		var textColor *color.Color
-		switch logType {
-		case Info:
-			textColor = color.New(color.FgGreen)
-		case Warn:
-			textColor = color.New(color.FgYellow)
-		case Error:
-			textColor = color.New(color.FgRed)
-		default:
-			textColor = color.New(color.Reset)
-		}
+    var textColor *color.Color
+    switch logType {
+    case Info:
+        textColor = color.New(color.FgGreen)
+    case Warn:
+        textColor = color.New(color.FgYellow)
+    case Error:
+        textColor = color.New(color.FgRed)
+    default:
+        textColor = color.New(color.Reset)
+    }
 
-		logMessageColored := textColor.SprintFunc()(logMessage)
-		fmt.Print(logMessageColored) // Imprime com cores
+    logMessageColored := textColor.SprintFunc()(logMessage)
+    fmt.Print(logMessageColored) // Imprime com cores
 
-		if l.logFile != nil {
-			_, err := l.logFile.WriteString(logMessage)
-			if err != nil {
-				return err // Retornar erro se ocorrer um problema ao escrever no arquivo de log
-			}
-		}
+    if l.logFile != nil {
+        _, err := l.logFile.WriteString(logMessage)
+        if err != nil {
+            return err // Retornar erro se ocorrer um problema ao escrever no arquivo de log
+        }
+    }
 
-		return nil // Sem erro
-	}
+    return nil // Sem erro
 }
 
 // Info logs an informational message.
 func (l *Lagra) Info(ctx context.Context, message string) error {
-	return l.send(Info)(ctx, message)
+    return l.send(Info, message)
 }
 
 // Warn logs a warning message.
 func (l *Lagra) Warn(ctx context.Context, message string) error {
-	return l.send(Warn)(ctx, message)
+    return l.send(Warn, message)
 }
 
 // Error logs an error message.
 func (l *Lagra) Error(ctx context.Context, message string) error {
-	return l.send(Error)(ctx, message)
+    return l.send(Error, message)
 }
 
 func (l *Lagra) save() error {
-	l.logMutex.Lock()
-	defer l.logMutex.Unlock()
+    l.logMutex.Lock()
+    defer l.logMutex.Unlock()
 
-	if l.logFile == nil {
-		var err error
-		l.logFile, err = os.Create("log.lagra")
-		if err != nil {
-			return err // Retornar erro se ocorrer um problema ao criar o arquivo de log
-		}
-	}
-	return nil // Sem erro
+    if l.logFile == nil {
+        var err error
+        l.logFile, err = os.Create("log.lagra")
+        if err != nil {
+            return err // Retornar erro se ocorrer um problema ao criar o arquivo de log
+        }
+    }
+    return nil // Sem erro
 }
 
 func (l *Lagra) Close() error {
-	l.logMutex.Lock()
-	defer l.logMutex.Unlock()
+    l.logMutex.Lock()
+    defer l.logMutex.Unlock()
 
-	if l.logFile != nil {
-		err := l.logFile.Close()
-		if err != nil {
-			return err // Retornar erro se ocorrer um problema ao fechar o arquivo de log
-		}
-	}
-	return nil // Sem erro
+    if l.logFile != nil {
+        err := l.logFile.Close()
+        if err != nil {
+            return err // Retornar erro se ocorrer um problema ao fechar o arquivo de log
+        }
+    }
+    return nil // Sem erro
 }
